9c9c4223dd830d8321a0bb8a893937a9f863b38d
parth-bhavsar <parthbhavsar80@gmail.com>
2023-04-16 18:45:10 -0400

    Initial Commit

---
+++ JoyRide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JoyRide
+//
+//  Created by Parth Bhavsar on 4/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
